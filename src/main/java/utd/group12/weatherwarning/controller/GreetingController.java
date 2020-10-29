2088a18e340085b2bd85168d36030eddd2abe9b6
ParkerMc <ParkerMc@users.noreply.github.com>
2020-10-29 14:34:23 -0500

    Added /api prefix to endpoint

--- src/main/java/utd/group12/weatherwarning/controller/GreetingController.java
+++ src/main/java/utd/group12/weatherwarning/controller/GreetingController.java
@@ -1,11 +1,11 @@
 /*
- * GreetingController registers the endpoint "/greeting" and returns the a Greeting object that will be converted to json automatically
- * To test it you can run the program and in your web browser go to "http://localhost:8080/greeting"
+ * GreetingController registers the endpoint "/api/greeting" and returns the a Greeting object that will be converted to json automatically
+ * To test it you can run the program and in your web browser go to "http://localhost:8080/api/greeting"
  * 
  * you can also add parameters to the url, see examples below
- * http://localhost:8080/greeting?name=Test
- * http://localhost:8080/greeting?service_name=Test_Service
- * http://localhost:8080/greeting?name=Test&service_name=Test_Service
+ * http://localhost:8080/api/greeting?name=Test
+ * http://localhost:8080/api/greeting?service_name=Test_Service
+ * http://localhost:8080/api/greeting?name=Test&service_name=Test_Service
  */
 package utd.group12.weatherwarning.controller;
 
@@ -21,7 +21,7 @@ public class GreetingController {
 
 	private static final String template = "Hello, %s!";
 
-	@GetMapping("/greeting")		// Register this method with the endpoint (can have multiple function per class)
+	@GetMapping("/api/greeting")		// Register this method with the endpoint (can have multiple function per class)
 	public Greeting greeting(@RequestParam(value = "service_name", defaultValue = "Weather Warning") String service_name,	// add param @RequestParam registers it so it will get the pramiter from the http request
 			@RequestParam(value = "name", defaultValue = "World") String name) {			 // Second param
 		// More code can run here
